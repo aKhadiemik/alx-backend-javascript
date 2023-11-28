ec6957cdb5c456069ddf2b4237974a65abf5b1b8
Ngarotosto <carolvsreyesz@gmail.com>
2023-11-28 04:11:52 +0300

    doc: comments added for modified conditional

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+/*start conditional*/
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+/*end cnoditional*/
 
   return [task, task2];
 }
