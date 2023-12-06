bb92c673e23cf8403fbf39cfcfadfe011b3beb3c
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-06 05:03:01 +0300

    add: script contains function getListStudents that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const getListStudents =() => {
+  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  const newarr = [];
+  newarr.push(student1, student2, student3);
+  return newarr;
+};
+
+export default getListStudents;
