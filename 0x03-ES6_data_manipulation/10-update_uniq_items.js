d629f14157b6ef0d7256f40afa71438f540768fc
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-06 05:30:41 +0300

    fix: implementation for updateUniqueItems changed so that it is not const but function

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,13 @@
-const updateUniqueItems = (map) => {
-    map.forEach((value) => {
+function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
+      }
+  
+    for (const [key, value] of map.entries()) {
         if (value === 1) {
-            return map.set(value, 100)
+            map.set(key, 100);
         }
-    })
+    }
 }
 
 export default updateUniqueItems
