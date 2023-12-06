ec758fca78aaef5a37ce4bb88484d46b7a9cf1c6
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-06 05:23:59 +0300

    fix: edited value required for update of unique items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 const updateUniqueItems = (map) => {
     map.forEach((value) => {
         if (value === 1) {
-            return map.set(value, 123)
+            return map.set(value, 100)
         }
     })
 }
